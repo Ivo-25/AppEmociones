9a251253da31f27d2cfbc2506957765284c04f1b
alemasmut96 <59920707+alemasmut96@users.noreply.github.com>
2020-03-10 12:43:54 -0300

    App emociones

---
+++ app/src/main/java/com/example/appprueba/Frase.java
@@ -0,0 +1,23 @@
+package com.example.appprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Frase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frase);
+    }
+
+    // Método para el botón Átras
+
+    public void Anterior (View view) {
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
+    }
+}
